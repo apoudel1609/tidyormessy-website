f24fa02df73eafb8a6daf760f531027b2bcd019a
Anup <apoudel225@gmail.com>
2024-06-22 23:01:27 -0500

    Initial Commit

---
+++ Room/RoomApp.swift
@@ -0,0 +1,32 @@
+//
+//  RoomApp.swift
+//  Room
+//
+//  Created by Anupam on 6/22/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RoomApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
